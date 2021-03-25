a8e91208f178af6b54d3401f6a11c6cfa706b328
Salman Waheed <mkdirenv@gmail.com>
2021-03-25 15:45:56 +0400

    fix builds

--- test/test.js
+++ test/test.js
@@ -2,18 +2,7 @@
 
 const request = require('supertest');
 const assert = require('assert');
-const app1 = require('../index.js');
-const random_test = require('../random_test.js');
-
-describe('GET /', () => {
-  it('respond with json', (done) => {
-    request(app1)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  });
-});
+const random_test = require('./random_test.js');
 
 describe('#random_test()', () => {
   it('random_test result', (done) => {
