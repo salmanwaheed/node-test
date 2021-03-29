e2fde6dc7bf93d5246da42d476c746b72bd1b67b
Salman Waheed <15670072+salmanwaheed@users.noreply.github.com>
2021-03-29 16:25:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-  res.json({ message: "Welcome to the API......" })
+  res.json({ message: "Welcome to the API." })
 })
 
 app.use((req, res, next) => {
